c5c8ae5aa8432ba8c3be71b4575eba9ea82af50e
Matheus <giehlmatheus96@gmail.com>
2022-07-19 21:54:49 -0300

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
-</template>
+<templete>
+  <section class="todoapp">
+  <header class="header">
+     <h1>Tarefas</h1>
+     <input-task @newTask="addTask"></input-task>
+     <task-list :todo-list="tasks"></taks-list>
+  </<header>  
+  </section>
+</templete>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+button {
+  margin: 0;
+  padding: 0;
+  background: none;
+  font-size: 100%;
+  vertical-aling: baseline;
+  font-family: inherit;
+  font-weight:inherit;
+  color: inherit; 
+  
 }
 </style>
