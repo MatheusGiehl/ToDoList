eccdf06e7cfb534d43eed94b1795011d1362507c
Matheus <giehlmatheus96@gmail.com>
2022-07-29 21:16:30 -0300

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,55 @@ button {
   vertical-aling: baseline;
   font-family: inherit;
   font-weight:inherit;
-  color: inherit; 
+  color: inherit;
+  -webkit-appearance: none;
+  appearance: none;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale; 
+}
+
+body {
+  font: 14px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  line-height: 1.4rem;
+  background: #F5F5F5;
+  color: #4D4D4D;
+  min-width: 230px;
+  max-width: 550px;
+  margin: 0 auto;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-weight: 300;
+}
+
+:focus {
+  outline: 0;
+}
+
+.cep {
+  text-decoration: none;
+  font-size: 16px;
+  display: block;
+  padding: 5px;
+  border-top:1px solid #EDEDED;
+  text-aling: center;
+}
+
+.hiden {
+  display: none;
+}
+
+.todoapp {
+  background: #FFF;
+  margin: 130px 0 40px 0;
+  position: relative;
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,0.2),
+              0 25px 50px 0 rgba(0,0,0,0.2)      
+}
+
+.todoapp h1 {
+  position: absolute;
+  top: -155px;
   
 }
+
 </style>
